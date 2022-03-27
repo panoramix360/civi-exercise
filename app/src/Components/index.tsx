d654a136af21f1c6fda45ef05fb32223a24f6678
lucas.reis <lucasreis3000@gmail.com>
2022-03-27 20:25:02 -0300

    creating detail screen

--- app/src/Components/index.tsx
+++ app/src/Components/index.tsx
@@ -1,3 +1,4 @@
 export { default as Container } from './Container'
 export { default as Logo } from './Logo'
 export { default as ListItem } from './ListItem'
+export { default as Field } from './Field'
